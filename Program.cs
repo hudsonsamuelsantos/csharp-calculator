8a7040b25f60caca29fb276bbe77503620495315
Hudson Santos <hudsnss@gmail.com>
2024-11-18 21:09:43 -0300

    functions refac

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿Console.Clear();
-Console.WriteLine("Digite o primeiro número: ");
-int primeiroNumero = int.Parse(Console.ReadLine());
-Console.Clear();
-// Console.WriteLine(primeiroNumero);
+﻿int primeiroNumero = PrimeiroInput();
+int segundoNumero = SegundoInput();
+string operador = SelecaoDeOperador();
+Resultado(operador, primeiroNumero, segundoNumero);
 
-Console.WriteLine(
+static int PrimeiroInput()
+{
+    Console.Clear();
+    Console.WriteLine(
+        @"Bem vindo a sua calculadora!
+
+        Digite o primeiro número: "
+        );
+    int primeiroNumero = int.Parse(Console.ReadLine());
+    Console.Clear();
+
+    return primeiroNumero;
+}
+
+static string SelecaoDeOperador()
+{
+    Console.WriteLine(
     @"Diga qual operador deseja usar. Digite:
     + (Para soma)
     - (Para multiplicação)
@@ -13,33 +28,48 @@ Console.WriteLine(
     
     Digite o operador: "
 );
-string operador = Console.ReadLine();
-// Console.WriteLine(operador);
-Console.Clear();
+    string operador = Console.ReadLine();
 
-Console.WriteLine("Digite o segundo número: ");
-int segundoNumero = int.Parse(Console.ReadLine());
-Console.Clear();
-// Console.WriteLine(segundoNumero);
+    Console.Clear();
 
-int resultado = 0;
-switch (operador)
-{
-    case "+":
-        resultado = primeiroNumero + segundoNumero;
-        break;
-    case "-":
-        resultado = primeiroNumero - segundoNumero;
-        break;
-    case "*":
-        resultado = primeiroNumero * segundoNumero;
-        break;
-    case "/":
-        resultado = primeiroNumero / segundoNumero;
-        break;
-    default:
-        Console.WriteLine("Operador inválido tente novamente.");
-        break;
+    return operador;
 }
 
-Console.WriteLine($"Resultado: {primeiroNumero} {operador} {segundoNumero} = {resultado}");
+static int SegundoInput()
+{
+    Console.WriteLine("Digite o segundo número: ");
+    int segundoNumero = int.Parse(Console.ReadLine());
+    Console.Clear();
+
+    return segundoNumero;
+}
+
+static void Resultado(string operador, int primeiroNumero, int segundoNumero)
+{
+    int resultado = 0;
+    bool operadorValido = true;
+    switch (operador)
+    {
+        case "+":
+            resultado = primeiroNumero + segundoNumero;
+            break;
+        case "-":
+            resultado = primeiroNumero - segundoNumero;
+            break;
+        case "*":
+            resultado = primeiroNumero * segundoNumero;
+            break;
+        case "/":
+            resultado = primeiroNumero / segundoNumero;
+            break;
+        default:
+            operadorValido = false;
+            break;
+    }
+
+    if (operadorValido)
+        Console.WriteLine($"Resultado: {primeiroNumero} {operador} {segundoNumero} = {resultado}");
+    else
+        Console.WriteLine("Operador inválido! Tente novamente!");
+
+}
